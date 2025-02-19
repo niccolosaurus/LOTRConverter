fcbd63937605492b2e3b8cd5557b216ec957db8d
Niccolo <nicco.eck@gmail.com>
2025-02-18 17:27:40 -0800

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Niccolo Eck on 2/18/25.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
