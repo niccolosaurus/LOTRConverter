fbd25a0cc2d7d2c043119ba81b068f37feaf605c
Niccolo <nicco.eck@gmail.com>
2025-04-24 16:18:25 -0700

    created the 'ExchangeInfo' doc for when we click on the info button

---
+++ LOTRConverter/ExchangeInfo.swift
@@ -0,0 +1,18 @@
+//
+//  ExchangeInfo.swift
+//  LOTRConverter
+//
+//  Created by Niccolo Eck on 4/24/25.
+//
+
+import SwiftUI
+
+struct ExchangeInfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExchangeInfo()
+}
