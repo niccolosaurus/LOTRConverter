25cb04574551892b300d0e0e0242abb56066cbf9
Niccolo <nicco.eck@gmail.com>
2025-04-24 16:29:06 -0700

    added in the title, text and the first HStack and conversion

--- LOTRConverter/ExchangeInfo.swift
+++ LOTRConverter/ExchangeInfo.swift
@@ -11,19 +11,38 @@ struct ExchangeInfo: View {
     var body: some View {
         ZStack{
             // Background Parchment image
+            Image(.parchment)
+                .resizable()
+                .ignoresSafeArea()
+                .background(.brown)
             
             VStack{
                 // title text
+                Text("Exchange Rates")
+                    .font(.largeTitle)
+                    .tracking(3)
                 
                 // description text
+                Text("Here at the Prancing Pony, we are happy to offer you a place where you can exchange all the known currencies in the entire world except one. We used to take Brandy Bucks, but after finding out that it was a person instead of a piece of paper, we realized it had no value to us. Below is a simple guide to our currency exchange rates:")
+                    .font(.title3)
+                    .padding()
                 
                 // exchagne rates
                 HStack{
                     // left currency image
+                    Image(.goldpiece)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 33)
                     
                     // exchange rate text
+                    Text("1 Gold Piece = 4 Gold Pennies")
                     
                     // right currency image
+                    Image(.goldpenny)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 33)
                 }
             }
         }
