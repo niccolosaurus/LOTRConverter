b2a048d92b2e0f51af61aa84cb533ce72745e32f
Niccolo <nicco.eck@gmail.com>
2025-05-02 20:23:08 -0700

    plugged in our declaration for the 'CurrencyIcon'

--- LOTRConverter/SelectCurrency.swift
+++ LOTRConverter/SelectCurrency.swift
@@ -24,23 +24,7 @@ struct SelectCurrency: View {
                     .fontWeight(.bold)
                 
                 // Currency icons
-                ZStack(alignment: .bottom) {
-                    // Currency Image
-                    Image(.copperpenny)
-                        .resizable()
-                        .scaledToFit()
-                    
-                    // Currency Name
-                    Text("Copper Penny")
-                        .padding(3)
-                        .font(.caption)
-                        .frame(maxWidth: .infinity)
-                        .background(.brown.opacity(0.75))
-                }
-                .padding(3)
-                .frame(width: 100, height: 100)
-                .background(.brown)
-                .clipShape(.rect(cornerRadius: 25))
+                CurrencyIcon(currencyImage: .copperpenny, currencyName: "Copper Penny")
                 
                 // Text
                 Text("Select the cuurency you would like to convert to:")
