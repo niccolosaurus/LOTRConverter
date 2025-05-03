d06c224ed4a908bb1e2ab8accb7d34ab21deebf9
Niccolo <nicco.eck@gmail.com>
2025-05-02 19:42:17 -0700

    Created the SelectCurrency view

---
+++ LOTRConverter/SelectCurrency.swift
@@ -0,0 +1,18 @@
+//
+//  SelectCurrency.swift
+//  LOTRConverter
+//
+//  Created by Niccolo Eck on 4/24/25.
+//
+
+import SwiftUI
+
+struct SelectCurrency: View {
+    var body: some View {
+    
+    }
+}
+
+#Preview {
+    SelectCurrency()
+}
