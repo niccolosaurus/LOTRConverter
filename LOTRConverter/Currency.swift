ccb19b5292b9364933a0888a35a0660b60537a1f
Niccolo <nicco.eck@gmail.com>
2025-08-25 20:19:55 -0700

    gave values to our currency, based on the goldPiece

--- LOTRConverter/Currency.swift
+++ LOTRConverter/Currency.swift
@@ -6,9 +6,9 @@
 //
 
 enum Currency: Double {
-    case copperPenny
-    case silverPenny
-    case silverPiece
-    case goldPenny
-    case goldPeice
+    case copperPenny = 6400
+    case silverPenny = 64
+    case silverPiece = 16
+    case goldPenny = 4
+    case goldPeice = 1
 }
