ca0677e93d236ace6e5658256c8beb82f37b5aaf
Niccolo <nicco.eck@gmail.com>
2025-08-25 20:12:09 -0700

    created Currency, then added a enum to it named Currency

---
+++ LOTRConverter/Currency.swift
@@ -0,0 +1,14 @@
+//
+//  Currency.swift
+//  LOTRConverter
+//
+//  Created by Niccolo Eck on 8/25/25.
+//
+
+enum Currency: Double {
+    case copperPenny
+    case silverPenny
+    case silverPiece
+    case goldPenny
+    case goldPeice
+}
